e2d52b9ec6fe3cb373a93d5c5e1a456dda48e624
Sirshak Bohara <boharasirshak101@gmail.com>
2024-02-29 23:17:10 +0545

    feat: added a .env parser to read .env variables.

---
+++ AvitoAdsParser/Parsers/EnvParser.cs
@@ -0,0 +1,26 @@
+﻿namespace AvitoAdsParser.Parsers;
+
+public static class EnvParser
+{
+    public static void Load(string filePath)
+    {
+        if (!File.Exists(filePath))
+            throw new FileNotFoundException("File not found", filePath);
+
+        foreach (var line in File.ReadAllLines(filePath))
+        {
+            var parts = line.Split('=');
+            if (parts.Length < 2)
+            {
+                continue;
+            }
+
+            if (parts.Length > 2)
+            {
+                parts[1] = string.Join('=', parts.Skip(1));
+            }
+
+            Environment.SetEnvironmentVariable(parts[0], parts[1]);
+        }
+    }
+}
