77b7b16b90d07a8b0ce31724fc160378dafaaf67
Sirshak Bohara <boharasirshak101@gmail.com>
2024-02-29 23:18:21 +0545

    feat: added a searches file containing the document class for mongodb searches table

---
+++ AvitoAdsParser/Models/Searches.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace AvitoAdsParser.Models;
+
+public abstract class SearchesDocument
+{
+    public ObjectId Id { get; set; }
+    
+    [BsonElement("user_id")]
+    public ObjectId UserId { get; set; }
+    
+    public string Query { get; set; } = default!;
+    
+    public int Interval { get; set; }
+
+    [BsonElement("last_search")]
+    [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+    public DateTime LastSearch { get; set; }
+}
